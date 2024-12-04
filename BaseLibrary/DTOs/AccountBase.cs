7a3f8640168cddfdc11cd94ef20c1b89d0b0abac
xcharag <oscaraguilarfut@gmail.com>
2024-12-04 17:05:04 -0400

    Feat: Login finished - The design of the login is finished - Make sure to only let the user login and none of the others functions available

--- BaseLibrary/DTOs/AccountBase.cs
+++ BaseLibrary/DTOs/AccountBase.cs
@@ -4,10 +4,11 @@ namespace BaseLibrary.DTOs;
 
 public class AccountBase
 {
-    [Required]
+    [Required(ErrorMessage = "Debe de Ingresar un Usuario")]
+    [StringLength(20)]
     public string? Username { get; set; }
     
     [DataType(DataType.Password)]
-    [Required]
+    [Required(ErrorMessage = "Debe de Ingresar su Clave/Contraseña")]
     public string? Password { get; set; }
 }
--- Client/Program.cs
+++ Client/Program.cs
@@ -6,12 +6,17 @@ using ClientLibrary.Helpers;
 using ClientLibrary.Services.Contracts;
 using ClientLibrary.Services.Implementations;
 using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.FluentUI.AspNetCore.Components;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7235") });
+builder.Services.AddHttpClient("SystemApiClient", client =>
+{
+    client.BaseAddress = new Uri("http://localhost:5232");
+});
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("http://localhost:5232") });
 
 builder.Services.AddAuthorizationCore();
 builder.Services.AddBlazoredLocalStorage();
@@ -19,5 +24,7 @@ builder.Services.AddScoped<GetHttpClient>();
 builder.Services.AddScoped<LocalStorageService>();
 builder.Services.AddScoped<AuthenticationStateProvider, CustomAuthenticationStateProvider>();
 builder.Services.AddScoped<IUserAccountService, UserAccountService>();
+builder.Services.AddScoped<ToastService>();
+builder.Services.AddFluentUIComponents();
 
 await builder.Build().RunAsync();
